9bdf6099c06443b84098fb68995c4f3e139700bf
Francesco Zuppichini <francesco.zuppichini@gmail.com>
2024-01-06 14:29:38 +0100

    removing deps injection

--- api/index.py
+++ api/index.py
@@ -1,64 +1,32 @@
 from contextlib import asynccontextmanager
-from typing import Annotated, List
-from fastapi import Depends, FastAPI 
+from fastapi import FastAPI
 import httpx
 from pydantic import BaseModel, Field
 
 API_URL = "https://icanhazdadjoke.com/"
 
+app = FastAPI()
 
-class HTTPClient:
-    client: httpx.AsyncClient = None
-
-    @classmethod
-    async def get_client(cls):
-        if cls.client is None:
-            cls.client = httpx.AsyncClient()
-        return cls.client
-
-
-async def get_http_client():
-    return await HTTPClient.get_client()
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    yield
-    if HTTPClient.client:
-        await HTTPClient.client.aclose()
-
-
-app = FastAPI(lifespan=lifespan)
-client = httpx.AsyncClient()
-
-HTTPClientDeps = Annotated[httpx.AsyncClient, Depends(get_http_client)]
-
+class Joke(BaseModel):
+    text: str = Field(description="The joke's text")
 
 @app.get("/")
 async def handler():
     return {"result": "Hello 👋!"}
 
-
-class Joke(BaseModel):
-    text: str = Field(description="The joke's text")
-
-
 @app.get("/api/jokes/random", description="Returns a random joke")
-async def handler_gpt(
-    http_client: HTTPClientDeps,
-) -> Joke:
+async def handler_gpt() -> Joke:
     headers = {
         "Accept": "application/json",
         "User-Agent": "My FastAPI app (https://myapp.com/contact)",
     }
-    resp = await http_client.get(API_URL, headers=headers)
+    async with httpx.AsyncClient() as client:
+        resp = await client.get(API_URL, headers=headers)
     data = resp.json()
     return Joke(text=data["joke"])
 
-
 @app.get("/api/jokes/search")
 async def search_jokes(
-    http_client: HTTPClientDeps,
     term: str,
     page: int = 0,
 ) -> list[Joke]:
@@ -67,6 +35,7 @@ async def search_jokes(
         "Accept": "application/json",
         "User-Agent": "My FastAPI app (https://myapp.com/contact)",
     }
-    resp = await http_client.get(url, headers=headers)
+    async with httpx.AsyncClient() as client:
+        resp = await client.get(url, headers=headers)
     data = resp.json()
     return [Joke(text=joke["joke"]) for joke in data["results"]]

61fe5362eed3380d1284b4d092a542c45d0ed541
Francesco Zuppichini <francesco.zuppichini@gmail.com>
2024-01-06 13:50:56 +0100

    fuck vercel

--- api/index.py
+++ api/index.py
@@ -1,7 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
-@app.get("/api/python")
-def hello_world():
-    return {"message": "Hello World"}
+class Joke(BaseModel):
+    text: str = Field(description="The joke's text")
+
+@app.post("/api/gpt", description="Returns a random joke" )
+async def handler_gpt(req: Request) -> Joke:
+    print(await req.json())
+    return Joke(
+        text="Hear about the new restaurant called Karma? There's no menu: You get what you deserve."
+    )

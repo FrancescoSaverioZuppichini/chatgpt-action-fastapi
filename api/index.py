bb5b58a55cad9b32f7462c0a0f8407ac87dd8cf2
Francesco Zuppichini <francesco.zuppichini@gmail.com>
2024-01-06 13:48:59 +0100

    diocane

--- api/index.py
+++ api/index.py
@@ -1,22 +1,7 @@
-from fastapi import FastAPI, Request
-from pydantic import BaseModel, Field
-
-
-class Joke(BaseModel):
-    text: str = Field(description="The joke's text")
+from fastapi import FastAPI
 
 app = FastAPI()
 
-
-
-@app.get("/")
-async def handler():
-    return {"hello": "there!"}
-
-
-@app.post("/api/gpt", description="Returns a random joke", response_model=Joke)
-async def handler_gpt(req: Request):
-    print(await req.json())
-    return Joke(
-        text="Hear about the new restaurant called Karma? There's no menu: You get what you deserve."
-    )
+@app.get("/api/python")
+def hello_world():
+    return {"message": "Hello World"}

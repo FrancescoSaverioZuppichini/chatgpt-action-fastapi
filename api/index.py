7e36805a76ecbca0856c6e9fe96b96eb7ae8618d
Francesco Zuppichini <francesco.zuppichini@gmail.com>
2024-01-06 14:02:39 +0100

    done

--- api/index.py
+++ api/index.py
@@ -4,10 +4,17 @@ from pydantic import BaseModel, Field
 
 app = FastAPI()
 
+
+@app.get("/")
+async def handler():
+    return {"result": "Hello 👋!"}
+
+
 class Joke(BaseModel):
     text: str = Field(description="The joke's text")
 
-@app.post("/api/gpt", description="Returns a random joke" )
+
+@app.post("/api/gpt", description="Returns a random joke")
 async def handler_gpt(req: Request) -> Joke:
     print(await req.json())
     return Joke(

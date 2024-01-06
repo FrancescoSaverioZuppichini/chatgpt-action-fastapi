b05ceaafdd35b69cd405e6f32a475c3f8b1eab0e
Francesco Zuppichini <francesco.zuppichini@gmail.com>
2024-01-06 14:50:45 +0100

    maledetto dio

--- api/index.py
+++ api/index.py
@@ -1,36 +1,8 @@
-from contextlib import asynccontextmanager
-from typing import Annotated, List
-from fastapi import Depends, FastAPI, Path, Request
-import httpx
-from pydantic import BaseModel, Field
+from fastapi import FastAPI
 
-API_URL = "https://icanhazdadjoke.com/"
+app = FastAPI()
 
 
-class HTTPClient:
-    client: httpx.AsyncClient = None
-
-    @classmethod
-    async def get_client(cls):
-        if cls.client is None:
-            cls.client = httpx.AsyncClient()
-        return cls.client
-
-
-async def get_http_client():
-    return await HTTPClient.get_client()
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    yield
-    if HTTPClient.client:
-        await HTTPClient.client.aclose()
-
-
-app = FastAPI(lifespan=lifespan)
-client = httpx.AsyncClient()
-
-HTTPClientDeps = Annotated[httpx.AsyncClient, Depends(get_http_client)]
-
-
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
